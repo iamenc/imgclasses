edc71f603d5faa01e2a783b72840debff16b2f19
iamenc163 <iamenc163@163.com>
2021-03-14 17:06:28 +0800

    vuehtml逻辑

--- main.go
+++ main.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	_ "imgclasses/routers"
+
 	beego "github.com/beego/beego/v2/server/web"
 )
 
 func main() {
+	beego.BConfig.WebConfig.TemplateLeft = ">>>"
+	beego.BConfig.WebConfig.TemplateRight = "<<<"
+
 	beego.Run()
 }
-
---
+++ static/js/imgclasses.js
@@ -0,0 +1,23 @@
+new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue.js!',
+      showImgUrl: "https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1473836766,4030812874&fm=26&gp=0.jpg",
+      items:[
+        {
+            id: "",
+            classes: "未分类",
+            url: "https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1473836766,4030812874&fm=26&gp=0.jpg",
+        },
+        {
+            id: "",
+            classes: "未分类",
+            url: "https://dss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1473836766,4030812874&fm=26&gp=0.jpg",
+        },
+      ],
+      buttons:[
+        "正常",
+        "异常",
+      ]
+    },
+})
